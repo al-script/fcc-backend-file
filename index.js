19c9c50033c74abbe99eb06e115ea085d780e222
al-script <alec.sorensen.as@gmail.com>
2024-08-31 13:35:44 -0700

    Merge project files

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+// init project
+require("dotenv").config();
+var express = require("express");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+var cors = require("cors");
 var app = express();
 
+// basic config and routing
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
-
+// listen for requests
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
+});
+
+// log requests in console
+app.use("/", function (req, res, next) {
+  console.log("+++");
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  console.log("---");
+  next();
+});
+
+// handle file analysis and json response
+app.post("/api/fileanalyse", function (req, res) {
+  let fileName = req.files.upfile.name;
+  let fileType = req.files.upfile.mimetype;
+  let fileSize = req.files.upfile.size;
+  console.log(fileName, fileType, fileSize);
+  res.json({ name: fileName, type: fileType, size: fileSize });
 });
